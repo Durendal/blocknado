23255809be2f3af2b787dd0aae36152a40d8fd2f
Brian Hewitt <sir.durendal@gmail.com>
2018-05-06 14:13:27 -0600

    Added market call to wrapper and moved to REST directory

---
+++ REST/blocknado_api.py
@@ -0,0 +1,15 @@
+import requests
+
+class BlocknadoREST:
+
+    def __init__(self, apiKey='', apiSecret=''):
+        self.baseURL = 'https://blocknado.com/api/v1/'
+        self.apiKey = apiKey
+        self.apiSecret = apiSecret
+
+    def publicAPICall(self, call, params={}):
+        data = requests.get("%s%s" % (self.baseURL, call), params=params)
+        return data.text
+
+    def markets(self):
+        return self.publicAPICall("markets");
