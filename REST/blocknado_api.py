da39fecdb69866344a28b6da3357c0419f24f4f6
Brian Hewitt <sir.durendal@gmail.com>
2018-05-06 17:50:35 -0600

    Added empty privateAPICall method and changed publicAPICall return to data.json()

--- REST/blocknado_api.py
+++ REST/blocknado_api.py
@@ -3,13 +3,16 @@ import requests
 class BlocknadoREST:
 
     def __init__(self, apiKey='', apiSecret=''):
-        self.baseURL = 'https://blocknado.com/api/v1/'
+        self.baseURL = 'https://blocknado.com/api/v1'
         self.apiKey = apiKey
         self.apiSecret = apiSecret
 
     def publicAPICall(self, call, params={}):
-        data = requests.get("%s%s" % (self.baseURL, call), params=params)
-        return data.text
+        data = requests.get("%s/%s" % (self.baseURL, call), params=params)
+        return data.json()
+
+    def privateAPICall(self, call, params={}):
+        pass
 
     def markets(self):
         return self.publicAPICall("markets");
