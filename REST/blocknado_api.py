34caa78b11f3a985aa5d8619f5c387325bd6404f
Brian Hewitt <sir.durendal@gmail.com>
2018-05-06 18:41:10 -0600

    Added exception and credential check to privateAPICall

--- REST/blocknado_api.py
+++ REST/blocknado_api.py
@@ -1,17 +1,30 @@
 import requests
 
+class UnauthenticatedException(Exception):
+    pass
+
 class BlocknadoREST:
 
     def __init__(self, apiKey='', apiSecret=''):
         self.baseURL = 'https://blocknado.com/api/v1'
-        self.apiKey = apiKey
-        self.apiSecret = apiSecret
+        self._apiKey = apiKey
+        self._apiSecret = apiSecret
+
+    def getCredentials(self):
+        return (self._apiKey, self._apiSecret)
+
+    def checkCredentialsSet(self):
+        if not self._apiKey or not self._apiSecret:
+            return False
+        return True
 
     def publicAPICall(self, call, params={}):
         data = requests.get("%s/%s" % (self.baseURL, call), params=params)
         return data.json()
 
     def privateAPICall(self, call, params={}):
+        if not self.checkCredentialsSet():
+            raise UnauthenticatedException("Must provide an API Key and Secret")
         pass
 
     def markets(self):
