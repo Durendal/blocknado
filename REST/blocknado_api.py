a510ccd053033563d9ccf2ef2d2f6cd4b08a8c97
Brian Hewitt <sir.durendal@gmail.com>
2018-05-07 16:37:12 -0600

    Added orderbook API Call

--- REST/blocknado_api.py
+++ REST/blocknado_api.py
@@ -2,6 +2,8 @@ import requests
 
 class UnauthenticatedException(Exception):
     pass
+class NonexistantMarketException(Exception):
+    pass
 
 class BlocknadoREST:
 
@@ -9,7 +11,7 @@ class BlocknadoREST:
         self.baseURL = 'https://blocknado.com/api/v1'
         self._apiKey = apiKey
         self._apiSecret = apiSecret
-
+        self._markets = ['btc-ltc', 'btc-rby', 'btc-dash', 'btc-vtc']
     def getCredentials(self):
         return (self._apiKey, self._apiSecret)
 
@@ -18,14 +20,23 @@ class BlocknadoREST:
             return False
         return True
 
-    def publicAPICall(self, call, params={}):
-        data = requests.get("%s/%s" % (self.baseURL, call), params=params)
+    def publicAPICall(self, call, params=[]):
+        url = "%s/%s" % (self.baseURL, call)
+        if len(params) > 0:
+            url = "%s/%s" % (url, '/'.join(params))
+        data = requests.get(url)
         return data.json()
 
-    def privateAPICall(self, call, params={}):
+    def privateAPICall(self, call, params=[]):
         if not self.checkCredentialsSet():
             raise UnauthenticatedException("Must provide an API Key and Secret")
         pass
 
     def markets(self):
         return self.publicAPICall("markets");
+
+    def orderbook(self, symbol):
+        if symbol not in self._markets:
+            raise NonexistantMarketException("The selected market: %s does not exist" % symbol)
+        symbol = [symbol]
+        return self.publicAPICall("orderbook", params=symbol)
